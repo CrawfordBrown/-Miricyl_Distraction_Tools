10907e8286c668bceeb01d5ad50c5a6832b60f7f
CrawfordBrown <crawford.brown1@outlook.com>
2023-04-23 15:29:38 +0100

    circles stacked ontop of each other

--- App.js
+++ App.js
@@ -2,25 +2,31 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
-//Get dimensions of window so the app can work on multiple devices
+// Get the device's width and height
 const {width, height} = Dimensions.get('window');
 
-//set variable to the size of half the width
+// Calculate the width of the circle to be a quarter of the device's width
 const circleWidth = width /4;
 
 
 export default function App() {
   return (
+
+    // Displays 8 circles 
     <View style={styles.container}>
+      
       {[0,1,2,3,4,5,6,7].map((item) => (
       <View
-       style={{
-      opacity: 0.1,
-      backgroundColor: "purple", 
-     width: circleWidth, 
-     height: circleWidth,
-     borderRadius: circleWidth / 2
-        }}
+        key={item}
+        style={{
+        opacity: 0.1,
+        backgroundColor: "purple", 
+       width: circleWidth, 
+       height: circleWidth,
+       borderRadius: circleWidth / 2,
+       //stack circles ontop of each other
+       ...StyleSheet.absoluteFill,
+     }}
       ></View>
       ))}
     </View>
