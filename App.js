ac4d783da584eab6edf34ce54b2005b73671a0a3
CrawfordBrown <crawford.brown1@outlook.com>
2023-04-23 15:20:27 +0100

    circle displayed

--- App.js
+++ App.js
@@ -1,22 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
+//Get dimensions of window so the app can work on multiple devices
 const {width, height} = Dimensions.get('window');
+
+//set variable to the size of half the width
+const circleWidth = width /2;
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View
+       style={{
+      opacity: 0.1,
+      backgroundColor: "purple", 
+     width: circleWidth, 
+     height: circleWidth,
+     borderRadius: circleWidth / 2
+        }}
+      ></View>
     </View>
   );
-}
+      }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
